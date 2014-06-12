147c5d8938e2bb7328a7a96c102605eb9cebc2f0
Flavio De Stefano <destefano.flavio@gmail.com>
2014-06-12 21:54:55 +0200

    Fix color

--- siriwave.js
+++ siriwave.js
@@ -20,11 +20,11 @@ function SiriWave(opt){
 	this.color = (function hex2rgb(hex){
 		var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
 		hex = hex.replace(shorthandRegex, function(m,r,g,b) { return r + r + g + g + b + b; });
-    	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 		return result ?
 		parseInt(result[1],16).toString()+','+parseInt(result[2], 16).toString()+','+parseInt(result[3], 16).toString()
 		: null;
-	})(opt.color) || '255,255,255';
+	})(opt.color || '#fff') || '255,255,255';
 
 	var ratio = opt.ratio ? opt.ratio : ( window.devicePixelRatio ? window.devicePixelRatio : 1 );
 	this.width = ratio * (this.opt.width || 320);
