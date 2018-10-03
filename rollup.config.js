d9410cd56ffd1e07f338b46f628a2a232668dbb9
Flavio De Stefano <destefano.flavio@gmail.com>
2018-10-03 20:45:43 +0200

    Using IIFE instead

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,23 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 import livereload from 'rollup-plugin-livereload';
 import serve from 'rollup-plugin-serve';
 
 import pkg from './package.json';
+
+serve();
+livereload({
+	watch: 'dist'
+});
 
 export default [{
 		input: 'src/siriwave.js',
 		output: {
 			file: pkg.browser,
 			name: pkg.library,
-			format: 'umd'
+			format: 'iife'
 		},
 		plugins: [
 			resolve(),
@@ -19,18 +25,12 @@ export default [{
 			babel({
 				exclude: 'node_modules/**'
 			}),
-			serve(),
-			livereload({
-				watch: 'dist'
-			}),
+			uglify()
 		]
 	},
 	{
 		input: 'src/siriwave.js',
-		output: [{
-				file: pkg.main,
-				format: 'cjs'
-			},
+		output: [
 			{
 				file: pkg.module,
 				format: 'es'
